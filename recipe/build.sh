87647cbf288adb3e6efc227ef01c28d5a1b18fc2
Nishidha Panpaliya <npanpa23@in.ibm.com>
2022-01-19 04:58:08 -0600

    Initial changes for p10 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+# *****************************************************************
+# (C) Copyright IBM Corp. 2022. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *****************************************************************
 
-set -e -u
+set -ex
 
+PATH_VAR="$PATH"
+if [[ $ppc_arch == "p10" ]]
+then
+  export PATH=/opt/rh/gcc-toolset-11/root/usr/bin/:$PATH
+fi
+
+echo `which gcc`
+exit 1
 if [[ ${target_platform} != linux-aarch64 ]]; then
     # Stuart's recommendation to stop lapack-test from failing
     ulimit -s 50000
@@ -183,3 +206,6 @@ cp "${RECIPE_DIR}"/site.cfg "${PREFIX}"/site.cfg
 echo library_dirs = ${PREFIX}/lib >> "${PREFIX}"/site.cfg
 echo include_dirs = ${PREFIX}/include >> "${PREFIX}"/site.cfg
 echo runtime_include_dirs = ${PREFIX}/lib >> "${PREFIX}"/site.cfg
+
+#Restore PATH variable
+export PATH="$PATH_VAR"
